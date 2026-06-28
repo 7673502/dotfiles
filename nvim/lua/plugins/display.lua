a5b4ac13706be0ac22b4fb66611b37b92d3a2141
Pramod Anandarao <panandarao@wisc.edu>
2026-06-28 12:08:18 -0500

    added system theme syncing and status line

--- nvim/lua/plugins/display.lua
+++ nvim/lua/plugins/display.lua
@@ -1,12 +1,18 @@
 return {
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
 	{
 		"catppuccin/nvim",
-		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.o.background = "light"
-			vim.cmd([[colorscheme catppuccin-latte]])
+			require("catppuccin").setup({
+				background = {
+					light = "latte",
+					dark = "mocha",
+				},
+			})
+			vim.cmd([[colorscheme catppuccin]])
 		end,
 	},
-	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{ "f-person/auto-dark-mode.nvim", opts = {} },
+	{ "nvim-lualine/lualine.nvim", opts = {} },
 }
