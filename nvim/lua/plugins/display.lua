7aea243e0bf2f37f66a46253cb39eb474166ae6c
Pramod Anandarao <panandarao@wisc.edu>
2026-06-28 14:04:44 -0500

    set theme lazy loading to false and forced lualine to use same theme

--- nvim/lua/plugins/display.lua
+++ nvim/lua/plugins/display.lua
@@ -3,6 +3,8 @@ return {
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
 		config = function()
 			require("rose-pine").setup({
 				background = {
@@ -14,5 +16,5 @@ return {
 		end,
 	},
 	{ "f-person/auto-dark-mode.nvim", opts = {} },
-	{ "nvim-lualine/lualine.nvim", opts = {} },
+	{ "nvim-lualine/lualine.nvim", opts = { theme = "rose-pine" } },
 }
