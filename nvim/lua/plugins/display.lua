a2396649dcba327b6ed33783631cd085731e541d
Pramod Anandarao <panandarao@wisc.edu>
2026-06-28 16:34:07 -0500

    Revert "no more themes"
    This reverts commit 93e07643aa0be00e39f514e452fa485c274ae88a.

--- nvim/lua/plugins/display.lua
+++ nvim/lua/plugins/display.lua
@@ -1,5 +1,21 @@
 return {
 	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				dark_variant = "moon",
+				styles = {
+					bold = false,
+					italic = false,
+				},
+			})
+			vim.cmd([[colorscheme rose-pine]])
+		end,
+	},
 	{ "f-person/auto-dark-mode.nvim", opts = {} },
-	{ "nvim-lualine/lualine.nvim" },
+	{ "nvim-lualine/lualine.nvim", opts = { theme = "rose-pine" } },
 }
