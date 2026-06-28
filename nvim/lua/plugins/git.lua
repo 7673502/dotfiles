784c5005a6ab18a3c3e3650ccc3e30b2b9d8860c
Pramod Anandarao <panandarao@wisc.edu>
2026-06-28 17:56:20 -0500

    added missing dependency for diffview

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,6 +2,7 @@ return {
 	{ "lewis6991/gitsigns.nvim", opts = {} },
 	{
 		"sindrets/diffview.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		keys = {
 			{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Git Diff Open" },
 			{ "<leader>gx", "<cmd>DiffviewClose<cr>", desc = "Git Diff Close" },
