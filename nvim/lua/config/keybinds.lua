d3f1d01f6e1ad23982c8bf534da39669365318a6
Pramod Anandarao <panandarao@wisc.edu>
2026-06-29 18:25:48 -0500

    made K just show hover doc rather than diagnostics first; gl is being used for diagnostic hover now

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,14 +1,3 @@
--- function that checks for diagnostics before showing hover doc (binded to K)
-local function smart_hover()
-	local line_diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
-	if next(line_diagnostics) then
-		vim.diagnostic.open_float({ focusable = false })
-	else
-		vim.lsp.buf.hover()
-	end
-end
-vim.keymap.set("n", "K", smart_hover, { desc = "Show diagnostic or hover tooltips" })
-
 -- rename symbols using F2
 vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
