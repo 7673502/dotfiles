0b458965a4ad3f57278aa1d9113ec493a2be5df5
Pramod Anandarao <panandarao@wisc.edu>
2026-07-02 20:38:27 -0500

    removed old oil keybind

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -3,6 +3,3 @@ vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
 -- Open floating diagnostic message on the current line
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
-
--- open oil using Ex
-vim.api.nvim_create_user_command("Ex", "Oil", {})
