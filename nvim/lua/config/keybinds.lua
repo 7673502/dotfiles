bd55565af2cbaa26ba0f5887c4d03a147b47a975
Pramod Anandarao <panandarao@wisc.edu>
2026-07-08 10:41:00 -0500

    added keybind to force 'gd' to go to definition instead of top of current file

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
 -- Open floating diagnostic message on the current line
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+
+-- Force gd to use the lsp to go to the definition of the word under the cursor
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
