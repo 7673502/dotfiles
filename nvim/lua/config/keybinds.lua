6fa2f9ec6a933cc8fd0a2db3f2904bf508f6f71a
Pramod Anandarao <panandarao@wisc.edu>
2026-07-14 09:26:11 -0500

    Revert "Add oil back as a plugin with ability to open using :Ex"
    This reverts commit a1c5a4cae4a1c89a2897a1f61a923ae5a0db9446.

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -6,6 +6,3 @@ vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show line diagnos
 
 -- Force gd to use the lsp to go to the definition of the word under the cursor
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
-
--- open oil using Ex
-vim.api.nvim_create_user_command("Ex", "Oil", {})
--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -19,8 +19,4 @@ return {
 			},
 		},
 	},
-	{
-		"stevearc/oil.nvim",
-		opts = { default_file_explorer = true },
-	},
 }
